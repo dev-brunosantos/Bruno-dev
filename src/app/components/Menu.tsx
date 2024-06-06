c8f11342e67fd5bfb679a7c8c83301aa0f2aae63
dev-brunosantos <brunosflauta@gmail.com>
2024-06-06 11:05:48 -0300

    Atualizações no MENU PRINCIPAL da aplicação

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -5,13 +5,21 @@ import { Cabecalho, LogoContainer, MenuLinks, MenuOpc, MenuPrincipal } from "@/s
 export const Menu = () => {
     const links: string[] = ['home', 'sobre', 'projetos', 'contatos']
     // const links: string[] = ['home', 'projetos', 'contatos']
-    const [linha, setLinha] = useState<string>('10px')
+    const [linha, setLinha] = useState<string>('5%')
+
+    // const handleEscolhePagina = (txt: string) => {
+    //     if (txt == "sobre") { return setLinha('145px') }
+    //     else if (txt == "projetos") { return setLinha('280px') }
+    //     else if (txt == "contatos") { return setLinha('412px') }
+    //     return setLinha('10px')
+    // }
 
     const handleEscolhePagina = (txt: string) => {
-        if (txt == "sobre") { return setLinha('145px') }
-        else if (txt == "projetos") { return setLinha('280px') }
-        else if (txt == "contatos") { return setLinha('412px') }
-        return setLinha('10px')
+        if(txt == "home") { return setLinha('5%') }
+        else if (txt == "sobre") { return setLinha('31.5%') }
+        else if (txt == "projetos") { return setLinha('56.5%') }
+        else if (txt == "contatos") { return setLinha('82.5%') }
+        // return setLinha('10%')
     }
 
     return (
