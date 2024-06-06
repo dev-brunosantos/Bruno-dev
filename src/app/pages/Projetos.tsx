90b2d0451ec4e29ba694a8a2a918bdbdfc7405c4
dev-brunosantos <brunosflauta@gmail.com>
2024-06-06 19:29:26 -0300

    Reestruturando o codigo da tela PROJETOS

--- src/app/pages/Projetos.tsx
+++ src/app/pages/Projetos.tsx
@@ -3,62 +3,11 @@ import React from "react"
 import { ContainerPage } from "../../styles/PageStyles"
 import { CardProjeto } from "../components/Cards/CardProjetos"
 
-import { FaCss3, FaHtml5 } from 'react-icons/fa'
-
 import ProjetosInfor from '../../json/Projetos.json'
 
 export default function Projetos() {
     return (
         <ContainerPage id="projetos" style={{ paddingTop: '80px' }}>
-            {/* <CardProjeto
-                titulo="Projeto 1"
-                descricao="Esse projeto foi criado usando as seguintes tecnologias"
-                projeto={() => alert("Ir para Projeto")}
-                className="card-projeto-desktop"
-                >
-                <h1>TESTE DE COMPONENTE</h1>
-            </CardProjeto>
-            <CardProjeto
-                titulo="Projeto 1"
-                descricao="Esse projeto foi criado usando as seguintes tecnologias"
-                projeto={() => alert("Ir para Projeto")}
-                className="card-projeto-desktop"
-                >
-                <h1>TESTE DE COMPONENTE</h1>
-            </CardProjeto>
-            <CardProjeto
-                titulo="Projeto 1"
-                descricao="Esse projeto foi criado usando as seguintes tecnologias"
-                projeto={() => alert("Ir para Projeto")}
-                className="card-projeto-desktop"
-                >
-                <h1>TESTE DE COMPONENTE</h1>
-            </CardProjeto>
-            <CardProjeto
-                titulo="Projeto 1"
-                descricao="Esse projeto foi criado usando as seguintes tecnologias"
-                projeto={() => alert("Ir para Projeto")}
-                className="card-projeto-desktop"
-                >
-                <h1>TESTE DE COMPONENTE</h1>
-            </CardProjeto>
-            <CardProjeto
-                titulo="Projeto 1"
-                descricao="Esse projeto foi criado usando as seguintes tecnologias"
-                projeto={() => alert("Ir para Projeto")}
-                className="card-projeto-desktop"
-                >
-                <h1>TESTE DE COMPONENTE</h1>
-            </CardProjeto>
-            <CardProjeto
-                titulo="Projeto 1"
-                descricao="Esse projeto foi criado usando as seguintes tecnologias"
-                projeto={() => alert("Ir para Projeto")}
-                className="card-projeto-desktop"
-                >
-                <h1>TESTE DE COMPONENTE</h1>
-            </CardProjeto> */}
-
             {
                 ProjetosInfor.map((projeto) => (
                     <CardProjeto key={projeto.nome}
