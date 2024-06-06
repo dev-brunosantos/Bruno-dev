efaceacaeb357127f9c95819f01a48d0199cb0a9
dev-brunosantos <brunosflauta@gmail.com>
2024-06-06 19:36:26 -0300

    Reestruturando o codigo do componente CardTecnologia

--- src/app/components/Cards/CardTecnologia.tsx
+++ src/app/components/Cards/CardTecnologia.tsx
@@ -1,5 +1,3 @@
-import { CardContainer, CardInfor } from "@/styles/CardsStyles"
-
 interface CardTecnologiaInterface {
     width?: string,
     height?: string,
@@ -11,13 +9,6 @@ export const CardTecnologia = ({
     width, height, link, children
 }: CardTecnologiaInterface) => {
     return (
-        // <div className="centralizar" style={{
-        //     width: width, height: height,
-        // }}>
-        //     <a href={link} target="_blank">
-        //         {children}
-        //     </a>
-        // </div>
         <a
             href={link}
             target="_blank"
