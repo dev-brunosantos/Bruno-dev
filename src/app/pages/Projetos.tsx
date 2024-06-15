97bab42f688007fad19e4f13676bb25193e2e842
dev-brunosantos <brunosflauta@gmail.com>
2024-06-15 10:38:27 -0300

    Iniciando busca dos dados de projetos da API do GitHub

--- src/app/pages/Projetos.tsx
+++ src/app/pages/Projetos.tsx
@@ -7,21 +7,30 @@ import ProjetosInfor from '../../json/Projetos.json'
 
 export default function Projetos() {
 
-    const [dados, setDados] = useState()
+    const [dados, setDados] = useState<any>([])
+
+    const FunctionTeste = async (projeto) => {
+        const linguagens = await fetch(`https://api.github.com/repos/dev-brunosantos/${projeto}/languages`)
+        const testando = await linguagens.json()
+
+        console.log(testando)
+    }
 
     useEffect(() => {
-        const consulta = fetch("https://api.github.com/users/dev-brunosantos/repos")
-            .then(res => res.json())
-            .then(resposta => {
-                resposta.forEach(dado => {
-                    setDados(dado)
-                })
-            })
-    })
+        async function BuscarDados() {
+            const consulta = await fetch("https://api.github.com/users/dev-brunosantos/repos")
+            const resposta = await consulta.json()
+
+            console.log(resposta)
+            setDados(resposta)
+        }
+
+        BuscarDados()
+    }, [])
 
     return (
         <ContainerPage id="projetos" style={{ paddingTop: '80px' }}>
-            {
+            {/* {
                 ProjetosInfor.map((projeto) => (
                     <CardProjeto key={projeto.nome}
                         titulo={projeto.nome}
@@ -32,8 +41,26 @@ export default function Projetos() {
                         <h1>Tecnologias</h1>
                     </CardProjeto>
                 ))
+            } */}
+
+            {
+                dados.map((dado: any) => (
+                    <CardProjeto key={dado.id}
+                        titulo={dado.name}
+                        descricao={dado.description}
+                        projeto={() => alert("Ir para Projeto")}
+                        className="card-projeto-desktop"
+                    >
+
+                        <div>
+                           <h1 onClick={async () => {
+                            await FunctionTeste(dado.languages_url)
+                           }} >Linguagens</h1>
+                        </div>
+                    </CardProjeto>
+                ))
             }
-            
+
         </ContainerPage>
     )
 }
