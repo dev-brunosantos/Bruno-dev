095b74a9d50c619fdc7712b2ed360c41d67785aa
dev-brunosantos <brunosflauta@gmail.com>
2024-06-15 17:07:21 -0300

    Melhorando o formulário de contato

--- src/app/components/Inputs.tsx
+++ src/app/components/Inputs.tsx
@@ -1,23 +1,27 @@
 import { Input } from "@/styles/ContatosStyles"
+import { ChangeEvent } from "react"
 
 interface InputsInterface {
     width: string,
     placeholder: string,
     color?: string,
     fontSize?: string,
+    type?: string,
+    required?: boolean,
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void
 }
 
 export const InputComponent = ({
-    width, placeholder, color, fontSize="16px"
+    width, placeholder, color, fontSize="16px", 
+    required=false, type="text", onChange
 }: InputsInterface) => {
     return (
         <Input
-            style={{
-                width: width,
-                color: color,
-                fontSize: fontSize
-            }}
+            style={{ width: width, color: color, fontSize: fontSize }}
             placeholder={placeholder}
+            type={type}
+            required={required}
+            onChange={onChange}
         />
     )
 }
--- src/app/pages/Contatos.tsx
+++ src/app/pages/Contatos.tsx
@@ -2,16 +2,23 @@ import { ContainerForm, ContainerTextArea, Formulario } from "@/styles/ContatosS
 import { ContainerPage } from "@/styles/PageStyles";
 import { InputComponent } from "../components/Inputs";
 import { Btn } from "../components/Botoes/Btn";
+import { useState } from "react";
 
 export default function Contatos() {
 
     const opcInput: string[] = ["Nome completo", "E-mail", "Telefone", "Titulo da Mensagem"]
 
+    const [nome, setNome] = useState<string>()
+    const [email, setEmail] = useState<string>()
+    const [telefone, setTelefone] = useState<string>()
+    const [titulo, setTitulo] = useState<string>()
+    const [mensagem, setMensagem] = useState<string>()
+
     return (
         <ContainerPage id="contatos" style={{ paddingTop: '80px' }}>
             <Formulario>
                 <ContainerForm>
-                    {
+                    {/* {
                         opcInput.map((opc) => (
                             <InputComponent
                                 key={opc}
@@ -19,19 +26,47 @@ export default function Contatos() {
                                 placeholder={opc}
                                 color="#fff"
                                 fontSize="14px"
+                                onChange={(e) => setNome(e.target.value)}
                             />
                         ))
-                    }
+                    } */}
+
+                    <InputComponent key={0} width="100%"
+                        placeholder={opcInput[0]}
+                        color="#fff" fontSize="14px"
+                        required={true}
+                        onChange={(e) => setNome(e.target.value)}
+                    />
+                    <InputComponent key={0} width="100%"
+                        placeholder={opcInput[1]} type="email"
+                        color="#fff" fontSize="14px"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <InputComponent key={0} width="100%"
+                        placeholder={opcInput[2]} type="number"
+                        color="#fff" fontSize="14px"
+                        onChange={(e) => setTelefone(e.target.value)}
+                    />
+                    <InputComponent key={0} width="100%"
+                        placeholder={opcInput[3]}
+                        color="#fff" fontSize="14px"
+                        onChange={(e) => setTitulo(e.target.value)}
+                    />
                 </ContainerForm>
                 <ContainerForm>
                     {/* <h3>Sua mensagem</h3> */}
                     <div style={{ width: '100%' }}>
-                        <ContainerTextArea placeholder="Digite sua mensagem"/>
+                        <ContainerTextArea placeholder="Digite sua mensagem" />
                     </div>
-                    <Btn 
+                    <Btn
                         width="100px" height="40px"
                         titulo="Enviar" fontSize=""
-                        onClick={() => alert('Funcionou')}
+                        onClick={() => alert(`
+                            Nome: ${nome},    
+                            Email: ${email},    
+                            Telefone: ${telefone},    
+                            Titulo da mensagem: ${titulo}    
+                        `)}
                     />
                 </ContainerForm>
             </Formulario>
